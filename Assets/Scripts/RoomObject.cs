269ff71f3a26572e9b27c08ea04ba49133238514
Dario <otrantodario@gmail.com>
2022-11-19 12:03:24 -0300

    Add Rooms

---
+++ Assets/Scripts/RoomObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public class RoomObject : MonoBehaviour
+{
+
+    public  List<WorldObject> wordList = new List<WorldObject>();
+
+    private void Awake()
+    {
+        wordList = GetComponentsInChildren<WorldObject>().ToList();
+    }
+    public void Update()
+    {
+        
+    }
+}
