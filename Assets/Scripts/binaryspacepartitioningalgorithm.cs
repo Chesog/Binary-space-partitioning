fac42115912b1a005f2a7715a2e7e5ea2f38bb6e
Dario <otrantodario@gmail.com>
2022-11-22 03:11:11 -0300

    Add set wordPoints && set wordPlanes

--- Assets/Scripts/binaryspacepartitioningalgorithm.cs
+++ Assets/Scripts/binaryspacepartitioningalgorithm.cs
@@ -23,9 +23,13 @@ public class binaryspacepartitioningalgorithm : MonoBehaviour
 
     void Update()
     {
-
+        setWordPoints();
+        setWordPlanes();
+    }
+    public void setWordPoints() 
+    {
         // Esquina principal
-        wordPoints[0] = new Vector3(transform.position.x - 10, transform.position.y, transform.position.z + 10);   
+        wordPoints[0] = new Vector3(transform.position.x - 10, transform.position.y, transform.position.z + 10);
         wordPoints[1] = new Vector3(transform.position.x - 10, transform.position.y + 5, transform.position.z + 10);
 
         // Esquina final primera fila
@@ -48,6 +52,31 @@ public class binaryspacepartitioningalgorithm : MonoBehaviour
 
         wordPoints[10] = new Vector3(transform.position.x - 10, transform.position.y, transform.position.z - 30);
         wordPoints[11] = new Vector3(transform.position.x - 10, transform.position.y + 5, transform.position.z - 30);
+
+        wordPoints[12] = new Vector3(transform.position.x + 10, transform.position.y, transform.position.z - 50);
+        wordPoints[13] = new Vector3(transform.position.x + 10, transform.position.y + 5, transform.position.z - 50);
+
+        wordPoints[14] = new Vector3(transform.position.x + 30, transform.position.y, transform.position.z - 50);
+        wordPoints[15] = new Vector3(transform.position.x + 30, transform.position.y + 5, transform.position.z - 50);
+
+        wordPoints[16] = new Vector3(transform.position.x + 50, transform.position.y, transform.position.z - 30);
+        wordPoints[17] = new Vector3(transform.position.x + 50, transform.position.y + 5, transform.position.z - 30);
+
+        wordPoints[18] = new Vector3(transform.position.x + 50, transform.position.y, transform.position.z - 10);
+        wordPoints[19] = new Vector3(transform.position.x + 50, transform.position.y + 5, transform.position.z - 10);
+
+        wordPoints[20] = new Vector3(transform.position.x + 10, transform.position.y, transform.position.z + 10);
+        wordPoints[21] = new Vector3(transform.position.x + 10, transform.position.y + 5, transform.position.z + 10);
+
+        wordPoints[22] = new Vector3(transform.position.x + 30, transform.position.y, transform.position.z + 10);
+        wordPoints[23] = new Vector3(transform.position.x + 30, transform.position.y + 5, transform.position.z + 10);
+    }
+    public void setWordPlanes() 
+    {
+        wallPlanes[0].Set3Points(wordPoints[1], wordPoints[3], wordPoints[2]);
+        wallPlanes[1].Set3Points(wordPoints[3], wordPoints[5], wordPoints[4]);
+        wallPlanes[2].Set3Points(wordPoints[5], wordPoints[7], wordPoints[6]);
+        wallPlanes[3].Set3Points(wordPoints[7], wordPoints[1], wordPoints[0]);
     }
     private void OnDrawGizmos()
     {
@@ -57,7 +86,7 @@ public class binaryspacepartitioningalgorithm : MonoBehaviour
         }
         Gizmos.color = Color.black;
 
-        for (int i = 0; i < 12; i++)
+        for (int i = 0; i < 24; i++)
         {
             Gizmos.DrawSphere(wordPoints[i], 0.5f);
         }
